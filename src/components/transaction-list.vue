01ca29cd17d49a4860a3762fa00a348464b53154
chukwumdiebube <chukwumdiebubeojinta@gmail.com>
2024-01-31 02:53:16 -0800

    initial commit

---
+++ src/components/transaction-list.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+      <li
+      >
+        transaction.text  <span>$</span
+        ><button class="delete-btn">
+          x
+        </button>
+      </li>
+    </ul>
+  </template>
